a005b5f595b0f20617fe2bf0052c19839a1b588a
tmh9x <thomas.hermanu@gmail.com>
2021-05-07 11:00:00 +0200

    Add functions

---
+++ utils/api.js
@@ -0,0 +1,11 @@
+export async function getCharacters(name) {
+    const promise = fetch(
+        `http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json`
+    );
+    const response = await promise;
+    if (response.status === 404) {
+        return {};
+    }
+    const data = await response.json();
+    return data;
+}
