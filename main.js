71d748f58ec038a1d72aa57cb37c896773d13967
tmh9x <thomas.hermanu@gmail.com>
2021-05-07 18:04:16 +0200

    ✨ Display champion infos and style

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import "./style.css";
 import { createElement } from "./utils/elements";
-// import { getChampions } from "./utils/api";
+import { getChampions } from "./utils/api";
+import { createChampionElement } from "./components/champion";
+import { debounce } from "./utils/timer";
+
+const championSection = createElement("main");
+getChampions().then((champions) => {
+  const championElements = Object.keys(champions).map((champion) =>
+    createChampionElement(champions[champion])
+  );
+  console.log(championElements);
+  championSection.append(...championElements);
+});
 
 const mainElement = createElement("main", {
   children: [
@@ -13,11 +24,21 @@ const mainElement = createElement("main", {
         }),
         createElement("input", {
           className: "input",
-          placeholder: "Search Champion",
+          placeholder: "Filter Champion",
           autofocus: true,
+          oninput: debounce((event) => {
+            const filter = event.target.value;
+            getChampions(filter).then((champions) => {
+              const championElements = champions
+                // .filter(champion)
+                .map(createChampionElement);
+              championSection.append(...championElements);
+            });
+          }, 300),
         }),
       ],
     }),
+    championSection,
     createElement("footer", {
       className: "footer",
       children: [
